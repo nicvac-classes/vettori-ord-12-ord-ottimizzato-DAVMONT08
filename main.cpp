00c8538457f8fcfbc59af6e695b797db914fb7b2
DAVMONT08 <davide.montella@marconibari.edu.it>
2025-09-26 23:05:06 +0000

    CONSEGNA

--- main.cpp
+++ main.cpp
@@ -1,11 +1,48 @@
 #include <iostream>
+#include <vector>
+#include <ctime>
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
-}
+              
+              int N,i,j,t;
+              bool scambio;
+              cout<<"quante cifre ci sono?"<<endl;
+              cin>>N;
+              vector<int>r[N];
+              for(i=0;i<N;i=i+1)
+              {
+              r[i]=random(N*10+1);
+              }
+              scambio=true;
+              for(i=0;i<N;i=i+1and scambio)
+              {
+                scambio=false;
+                j=0
+                while(j<=N-2)
+                {
+                    if(r[j]>r[j+1])
+                    {
+                        t=r[j];
+                        r[j]=r[j+1];
+                        r[j+1]=t;
+                        scambio=true;
+
+                    }
+                  j=j+1
+                }
+
+              }
+
+              i=0
+              while(i<N)
+              {
+                cout<<"r[i]"<<endl;
+                i=i+1
+
+            
+              }
+
+
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
